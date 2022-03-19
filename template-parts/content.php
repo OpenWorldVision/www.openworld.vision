57ad1ad00ba4b1b61c44da8f88063fa264060987
VinhTruong2001 <ducvinh.truong2001@gmail.com>
2022-03-19 11:52:53 +0700

    remove all file

--- template-parts/content.php
+++
@@ -1,9 +0,0 @@
-<article id="post-<?php echo esc_attr($id) ?>" <?php post_class(); ?>>
-  <div class="heading-block page-title"><?php the_title( '<h1 class="h">', '</h1>' ); ?></div>
-  <div class="content-area clearfix">
-    <?php the_content(''); ?>
-  </div>
-  <?php if(function_exists('wp_link_pages')) { ?>
-    <?php wp_link_pages(array('before' => '<div class="page-pagination">', 'after' => '</div>','link_before' => '<span>','link_after' => '</span>',)); ?>
-  <?php } ?>
-</article>
