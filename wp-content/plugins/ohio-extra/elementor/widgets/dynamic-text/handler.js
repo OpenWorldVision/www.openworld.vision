a59375cd1049fae3d335acc933de31c9cb306452
Tam Nguyen <nntam2013@gmail.com>
2022-12-15 16:13:30 +0700

    feat: new website

--- wp-content/plugins/ohio-extra/elementor/widgets/dynamic-text/handler.js
+++
@@ -1,7 +0,0 @@
-jQuery( window ).on('elementor/frontend/init', () => {
-    if (elementorFrontend.isEditMode()) {
-        elementorFrontend.hooks.addAction('frontend/element_ready/ohio_dynamic_text.default', ( $element ) => {
-            jQuery(window).trigger('ohio:handle_ohio_dynamic_text');
-        });
-    }
-});
